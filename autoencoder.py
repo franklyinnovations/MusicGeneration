0d4a7e01566586bc98227b45f3ead666a1055d11
jerryfan4 <jerry.fan1996@gmail.com>
2017-03-23 19:30:34 -0700

    Add autoencoder approach

--- autoencoder.py
+++ autoencoder.py
@@ -1,64 +1,97 @@
 import tensorflow as tf
 import numpy as np
 
-def encoder(x):
-	layer_1 = tf.nn.sigmoid(tf.add(tf,matmul(x, weights['encoder_h1']), bias['encoder_b1']))
-	layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), bias['encoder_b2']))
+def encoder(x, weights, bias):
+	layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['encoder_h1']), bias['encoder_b1']))
+	layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['encoder_h2']), bias['encoder_b2']))
 	
 	return layer_2
 
-def decoder(x):
-	layer_1 = tf.nn.sigmoid(tf.add(tf,matmul(x, weights['decoder_h1']), bias['decoder_b1']))
-	layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), bias['decoder_b2']))
+def decoder(x, weights, bias):
+	layer_1 = tf.nn.tanh(tf.add(tf.matmul(x, weights['decoder_h1']), bias['decoder_b1']))
+	layer_2 = tf.nn.tanh(tf.add(tf.matmul(layer_1, weights['decoder_h2']), bias['decoder_b2']))
 
 	return layer_2
 
-def autoencoder_melody(right, epochs=5):
+def preprocess(data):
 	min_length = np.inf
-	for sequence in right:
+	for sequence in data:
 		if len(sequence) < min_length:
 			min_length = len(sequence)
 
 	train_X = []
-	for sequence in right:
-		train_X.append(sequence[0:min_length])
+	means = []
+	stds = []
+	for sequence in data:
+		temp = np.array(sequence[0:min_length])
+		mean = np.mean(temp)
+		std = np.std(temp)
+		temp = (temp - mean) / std
+		temp = temp.reshape(1, min_length)
+		train_X.append(temp)
+		means.append(mean)
+		stds.append(std)
 
-	learning_rate = 0.01
-	num_hidden1 = 256
-	num_hidden2 = 128
-	num_input = min_length
+	return np.array(train_X), np.array(means), np.array(stds), min_length
 
-	X = tf.placeholder("float", [None, num_input])
+def postprocess(data, mean, std):
+	return np.round(data * std + mean)
+
+def generate_random_seed(dimension):
+	seed = np.random.normal(0, 1, dimension)
+	seed.astype(np.float64)
+	return seed
+
+def autoencoder_melody(right, epochs=5):
+	train_X, train_means, train_stds, train_length = preprocess(right[0:35])
+	test_X, test_means, test_stds, test_length = preprocess(right[35:40])
+
+	learning_rate = 0.02
+	num_hidden1 = 128
+	num_hidden2 = 64
+	num_input = train_length
 	weights = {
 		'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden1])),
 		'encoder_h2': tf.Variable(tf.random_normal([num_hidden1, num_hidden2])),
 		'decoder_h1': tf.Variable(tf.random_normal([num_hidden2, num_hidden1])),
-		'decoder_h1': tf.Variable(tf.random_normal([num_hidden1, num_input])),
-	}
+		'decoder_h2': tf.Variable(tf.random_normal([num_hidden1, num_input])),
+	}	
 	bias = {
-		'encoder_h1': tf.Variable(tf.random_normal([num_hidden1])),
-		'encoder_h2': tf.Variable(tf.random_normal([num_hidden2])),
-		'decoder_h1': tf.Variable(tf.random_normal([num_hidden2])),
-		'decoder_h1': tf.Variable(tf.random_normal([num_input])),	
+		'encoder_b1': tf.Variable(tf.random_normal([num_hidden1])),
+		'encoder_b2': tf.Variable(tf.random_normal([num_hidden2])),
+		'decoder_b1': tf.Variable(tf.random_normal([num_hidden1])),
+		'decoder_b2': tf.Variable(tf.random_normal([num_input])),	
 	}
+	X = tf.placeholder("float", [None, num_input])
 
-	encoder_op = encoder(X)
-	decoder_op = decoder(encoder_op)
+	encoder_op = encoder(X, weights, bias)
+	decoder_op = decoder(encoder_op, weights, bias)
 
 	y_pred = decoder_op
 	y_true = X
 
-	loss = tf.reduce_mean(tf.pow(y_true - y_pred), 2)
+	loss = tf.reduce_mean(tf.pow(y_true - y_pred, 2))
 	optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
 	init = tf.global_variables_initializer()
 
 	with tf.Session() as sess:
 		sess.run(init)
-		curr_loss = 0
 		for e in range(epochs):
+			curr_loss = 0
 			permutations = np.random.permutation(len(train_X))
 			for index in permutations:
 				o, l = sess.run([optimizer, loss], feed_dict={X: train_X[index]})
-				curr_loss += l
-			print (curr_loss)
+				curr_loss += (l/len(train_X))
+			print('Epoch ' + str(e) + ' training loss: ' + str(curr_loss))
+
+		output = []
+		for i in range(len(test_X)):
+			truncate = test_X[i][0]
+			truncate = truncate[0:train_length]
+			truncate = truncate.reshape(1, train_length)
+			encoder_decoder_sequence = sess.run(y_pred, feed_dict={X: truncate})
+			output.append(postprocess(encoder_decoder_sequence, test_means[i], test_stds[i]))
+		return output
+		
+
 
--- generate_music.py
+++ generate_music.py
@@ -1,14 +1,15 @@
 from music21 import *
 
 def generate_piano_midi(left, right):
+	normalizing_constant = 28
 	s = stream.Stream()
 	p0 = stream.Part()
 	p1 = stream.Part()
 	for n in right:
-		curr_note = note.Note(n + 28)
+		curr_note = note.Note(n + normalizing_constant)
 		p0.append(curr_note)
 	for n in left:
-		curr_note = note.Note(n + 28)
+		curr_note = note.Note(n + normalizing_constant)
 		p1.append(curr_note)
 	s.insert(0, p0)
 	s.insert(1, p1)
