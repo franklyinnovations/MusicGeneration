4787f0f2e3fc6ea4e4054ea5b9e1fa96b3ef0dae
jerryfan4 <jerry.fan1996@gmail.com>
2017-03-23 19:37:24 -0700

    Add normalizing constant for melody and accompany

--- generate_music.py
+++ generate_music.py
@@ -1,15 +1,16 @@
 from music21 import *
 
 def generate_piano_midi(left, right):
-	normalizing_constant = 28
+	normalizing_right_constant = 28
+	normalizing_left_constant = 21
 	s = stream.Stream()
 	p0 = stream.Part()
 	p1 = stream.Part()
 	for n in right:
-		curr_note = note.Note(n + normalizing_constant)
+		curr_note = note.Note(n + normalizing_right_constant)
 		p0.append(curr_note)
 	for n in left:
-		curr_note = note.Note(n + normalizing_constant)
+		curr_note = note.Note(n + normalizing_left_constant)
 		p1.append(curr_note)
 	s.insert(0, p0)
 	s.insert(1, p1)
